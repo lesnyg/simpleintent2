d729169ed324708479c602350c1dd61e50cff327
이선영 <lsy79@hanmail.net>
2018-10-29 15:39:50 +0900

    first commit

--- app/src/main/java/com/lesnyg/edu/simpleintent2/MainActivity.java
+++ app/src/main/java/com/lesnyg/edu/simpleintent2/MainActivity.java
@@ -1,13 +1,29 @@
 package com.lesnyg.edu.simpleintent2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonfirst = findViewById(R.id.buttonfirst);
+        buttonfirst.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonfirst:
+                Intent intent = new Intent(this,SecondActivity.class);
+                startActivity(intent);
+                break;
+        }
+
     }
 }
